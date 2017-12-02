29e5d19fe63a43c787c3f14da5c9c49bf964b98d
DANNNNYYYYYYYYYY-CISSMATEROV <danny2017313@concordiashanghai.org>
2017-12-02 18:06:47 -0500

    initial commit

--- yhacks17/yhacks17/urls.py
+++ yhacks17/yhacks17/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('yhackss17', admin.site.urls),
+    path('yhackss17', include('yhackss17.urls')),
 ]
--- yhacks17/yhackss17/views.py
+++ yhacks17/yhackss17/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("hello, world. You're at the yhack(ss)17")
+def text(request, text):
+    return HttpResponse(text)
