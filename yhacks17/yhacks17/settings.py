0ae5451930e733fe40c7f56a2cf5fb247ddcb3f3
DANNNNYYYYYYYYYY-CISSMATEROV <danny2017313@concordiashanghai.org>
2017-12-02 21:04:51 -0500

    final commit

--- yhacks17/yhacks17/settings.py
+++ yhacks17/yhacks17/settings.py
@@ -23,19 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ivzd^vv0=13#hes$36=@)0(8f2&php3ja2jnu)^0d%-n1x0mr^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
 
 # Application definition
 
@@ -126,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
